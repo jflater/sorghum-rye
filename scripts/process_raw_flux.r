75c1019c05903d243ad44d279f67a95c6003544d
jflater <jflater@gmail.com>
2025-07-22 11:32:43 -0500

    Add custom CSV reading function and data processing for flux data
    - Introduced a custom function to read CSV files, merging specific header rows.
    - Implemented data binding for multiple CSV files and cleaned column names.
    - Separated 'label_number' into 'plot' and 'location' columns.
    - Converted date column to Date format.
    - Added metadata reading and preparation for joining with flux data.

--- scripts/process_raw_flux.r
+++ scripts/process_raw_flux.r
@@ -1,5 +1,8 @@
 library("tidyverse")
-
+library("readr")
+library("dplyr")
+library("purrr")
+library("janitor")
 # Path to .csv files exported from LI-COR software
 data_path <- "sorghum-rye/data/processed/"
 
@@ -7,3 +10,32 @@ data_path <- "sorghum-rye/data/processed/"
 file_list <- list.files(path = data_path, pattern = "*.csv", full.names = TRUE)
 file_list
 
+# Read in all .csv files, skipping the first row and merging the second and third rows as the header
+
+
+custom_read_csv <- function(file) {
+  # Read first three rows
+  header_rows <- readr::read_csv(file, n_max = 3, col_names = FALSE, show_col_types = FALSE)
+  # Skip first row, merge row 2 and 3 with _
+  merged_names <- paste(header_rows[2, ], header_rows[3, ], sep = "_")
+  # Read the rest of the data, skipping first 3 rows, and set col_names
+  readr::read_csv(file, skip = 3, col_names = merged_names, show_col_types = FALSE)
+}
+
+flux_data <- map(file_list, custom_read_csv)
+
+head(flux_data[[1]])
+
+data <- bind_rows(flux_data) %>%
+  clean_names()
+
+# Separate label_number into plot and location on "_" or "-"
+data <- data %>%
+  separate(label_number, into = c("plot", "location"), sep = "[-_]") %>%
+  mutate(date = as.Date(date_time_initial_value_yyyy_mm_dd_hh_mm_ss, format = "%Y/%m/%d"))
+
+# metadata
+metadata <- read_csv("sorghum-rye/data/metadata/plot_treatments.csv", show_col_types = FALSE) %>%
+  clean_names()
+
+# Join data with metadata on plot and date columns
