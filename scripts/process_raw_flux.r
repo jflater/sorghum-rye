a89e7b0743d2b92bf18717ae1adb4abd18cfa3a0
jflater <jflater@gmail.com>
2025-07-22 11:07:46 -0500

    Refactor process_raw_flux script to include data path and file listing functionality

--- scripts/process_raw_flux.r
+++ scripts/process_raw_flux.r
@@ -1,1 +1,9 @@
 library("tidyverse")
+
+# Path to .csv files exported from LI-COR software
+data_path <- "sorghum-rye/data/processed/"
+
+# List all .csv files in the data_path directory
+file_list <- list.files(path = data_path, pattern = "*.csv", full.names = TRUE)
+file_list
+
