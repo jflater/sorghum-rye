f21e08c83b102a20deb2aaf0d9f653f8a650b698
jflater <jflater@gmail.com>
2025-08-14 10:27:47 -0500

    Add script to visualize daily precipitation for 2023 and 2024 with bar plot

---
+++ scripts/daily_rain_for_top_of_plots.r
@@ -0,0 +1,24 @@
+library(readxl)
+library(tidyverse)
+library(lubridate)
+rain_bar <- read_xlsx("data/rain/historical_rain.xlsx")
+
+# Filter to 2023 and 2024
+rain_bar <- rain_bar %>%
+  filter(year(day) %in% c(2023, 2024)) %>%
+  select(day, precipmm) %>%
+  mutate(day = as.Date(day))
+class(rain_bar$day)
+
+# bar plot with precipmm by day for each year
+rain_bar %>%
+  mutate(year = year(day)) %>%
+  ggplot(aes(x = day, y = precipmm, fill = as.factor(year))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Daily Precipitation in 2023 and 2024",
+       x = "Date",
+       y = "Precipitation (mm)",
+       fill = "Year") +
+  theme_minimal() +
+  scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "1 month") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
