3ac568664b4ffb6bb2f3f297fa7fd4ff1343d886
jflater <jflater@gmail.com>
2025-08-19 13:06:23 -0500

    Refactor daily precipitation plotting script for clarity and improved output handling

--- scripts/daily_rain_for_top_of_plots.r
+++ scripts/daily_rain_for_top_of_plots.r
@@ -11,14 +11,20 @@ rain_bar <- rain_bar %>%
 class(rain_bar$day)
 
 # bar plot with precipmm by day for each year
-rain_bar %>%
+p1 <- rain_bar %>%
   mutate(year = year(day)) %>%
   ggplot(aes(x = day, y = precipmm, fill = as.factor(year))) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Daily Precipitation in 2023 and 2024",
-       x = "Date",
-       y = "Precipitation (mm)",
-       fill = "Year") +
+  labs(
+    title = "Daily Precipitation in 2023 and 2024",
+    x = "Date",
+    y = "Precipitation (mm)",
+    fill = "Year"
+  ) +
   theme_minimal() +
   scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "1 month") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+p1
+p1$data %>%
+  write_csv("data/rain/daily_precip_2023_2024.csv")
