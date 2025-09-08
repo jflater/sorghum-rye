0c342b3e1877c5d85a7efa5369b9fc8e12b7f5da
jflater <jflater@gmail.com>
2025-09-08 15:08:57 -0500

    Add soil data processing script for 2023 and 2024 datasets

---
+++ scripts/clean_soil.R
@@ -0,0 +1,63 @@
+library(readxl)
+library(tidyverse)
+library(janitor)
+# Read in the two soil data files
+# pH and GWC might be hidden in other sheets
+soil_2023 <- read_xlsx("data/soils/2023_SABR_MASTER_soil.xlsx")
+soil_2024 <- read_xlsx("data/soils/2024_SABR_MASTER_soil.xlsx")
+
+soil_2023 <- soil_2023 %>%
+  select(-contains("...")) %>%
+  filter(
+    str_detect(`Project/sampling`, str_c("12-2023|Spring 2023")) &
+      !str_detect(`Plot ID`, str_c("NE|SE|SW|NW"))
+  ) %>%
+  rename(
+    plot = `Plot ID`,
+    nitrate_ppm = `Nitrate-ppm`,
+    ammonia_ppm = `Ammonia-ppm`
+  ) %>%
+  mutate(plot = str_pad(plot, width = 2, side = "left", pad = "0"))
+
+soil_2024 <- soil_2024 %>%
+  filter(
+    str_detect(lab_id, "2024") &
+      !str_detect(lab_id, str_c("NE|SE|SW|NW"))
+  ) %>%
+  separate(
+    lab_id,
+    into = c("date", "plot"),
+    sep = "_",
+    extra = "merge"
+  ) %>%
+  mutate(
+    plot = str_pad(plot, width = 2, side = "left", pad = "0"),
+    date = lubridate::ymd(date), # Use mdy() for MM/DD/YYYY or similar formats
+    ammonia_ppm = as.numeric(ammonia_ppm),
+    nitrate_ppm = as.numeric(nitrate_ppm)
+  ) %>%
+  select(date, plot, ammonia_ppm, nitrate_ppm)
+
+# Soil collection dates
+# There is a September date that need processed
+date_12_2023 <- as.Date("2023-12-14")
+date_spring_2023 <- as.Date("2023-05-18")
+
+# add date column with Soil collection dates
+soil_2023 <- soil_2023 %>%
+  mutate(
+    date = case_when(
+      str_detect(`Project/sampling`, "12-2023") ~ date_12_2023,
+      str_detect(`Project/sampling`, "Spring 2023") ~ date_spring_2023
+    ),
+    ammonia_ppm = as.numeric(ammonia_ppm),
+    nitrate_ppm = as.numeric(nitrate_ppm)
+  ) %>%
+  select(date, plot, ammonia_ppm, nitrate_ppm)
+
+# Compare df columns
+compare_df_cols(soil_2023, soil_2024)
+
+# Join the two data frames
+soil_combined <- bind_rows(soil_2023, soil_2024) %>%
+  arrange(date, plot)
